9542c3815ea6db0060a62760bdd71007b3e469eb
Karen <kchan190@uottawa.ca>
2025-02-16 22:47:13 -0500

    fixed github pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  
+  const testFunction2 = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/test-db');
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello
-        </p>
+        <button onClick={testFunction2}>Test database connection</button>
       </header>
     </div>
   );
